de72912093131819712a2e8d7949b7f94d91f329
orhan <orhantrkmn749@gmail.com>
2025-10-20 11:03:03 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">Tailwind Çalışıyor 🚀</h1>
+    </div>
+  );
+}
