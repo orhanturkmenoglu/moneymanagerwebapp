359c0ac1339c247dd20e85da10a10075ad2566b9
orhan <orhantrkmn749@gmail.com>
2025-10-23 12:05:35 +0300

    feat: add responsive Dashboard layout with Menubar and Sidebar integration

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Menubar from './Menubar'
+import { AppContext } from '../context/AppContext'
+import Sidebar from './Sidebar';
 
-const Dashboard = () => {
+const Dashboard = ({children,activeMenu}) => {
+    const {user} = useContext(AppContext);
   return (
     <div>
-        <Menubar/>
+        <Menubar activeMenu= {activeMenu}/>
+        {
+            user && (
+                <div className='flex'>
+                    <div className='max-[1080px]:hidden'>
+                        {/** side bar contet */}
+                        <Sidebar activeMenu= {activeMenu}/>
+                    </div>
+                    <div className='grow mx-5'>{children}</div>
+                </div>
+            )
+        }
     </div>
   )
 }
