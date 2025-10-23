b9b3802dde422ddf9dfa1250c254600a6a94e3c3
orhan <orhantrkmn749@gmail.com>
2025-10-23 12:06:18 +0300

    feat: add Expense page integrated with Dashboard layout

--- src/pages/Expense.jsx
+++ src/pages/Expense.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Dashboard from '../components/Dashboard'
 
 const Expense = () => {
   return (
-    <div>Expense</div>
+      <Dashboard activeMenu="Expense">
+        This Expense page 
+      </Dashboard>
   )
 }
 
