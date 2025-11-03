b68cc6b8020ad17c1da08c0a9c28b959a347a2f2
orhan <orhantrkmn749@gmail.com>
2025-11-03 10:28:31 +0300

    fix: Update BASE_URL to use production endpoint instead of local server

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = "https://money-manager-fullstack.onrender.com";
 
 export const CLOUDINARY_CLOUD_NAME = "dnxdum35d";
 
